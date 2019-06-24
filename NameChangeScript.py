8d7500bbc268b6ec679ce4c664b58ea9ca072e5d
danceofthejaberwocks <Suzanne.smith928@gmail.com>
2019-06-24 12:37:39 -0500

    Update NameChangeScript.py

--- NameChangeScript.py
+++ NameChangeScript.py
@@ -1,35 +1,37 @@
 from netmiko import Netmiko
-from run_pandas import *
+from ssh2 import *
 
-
-def name_change(dict):
+def name_change():
 ##Loop over the nested dictionary (that should be its own class, just for tidier files.) Once its looped define the host name and 
 #replace the 3- character prefix with the AD friendly 2-character prefix. The indents MUST stay as is, otherwise it will not loop properly
 ##for loops in python are similar to foreach loops in C# 
 ##K, V are for key and value 
-    file= input("What is the name of the file?")
-    f= open(file, 'r')
-    cisco_sw={}
-    cisco_sw['hosts']= {}
-    for line in f:
-        k,v= line.strip().split(',')
-        cisco_sw['hosts'][k.strip()]= v.strip()
-        f.close()
-    print(cisco_sw)
-    command= input("What commands would you like to execute?")
-    for k, v in test_dict.items() :
-        hostname=test_dict[k]['host']
+   
+    command=[]
+    getCommands= True
+   
+    while getCommands==True: 
+        getInput=input("Would you like to enter a command? Y or N")
+        gotInput= getInput.upper()
+        if gotInput== 'Y':
+            cli= input("What command would you like to enter?")
+            command.append(cli)
+        else: 
+            getCommands==False
+            break
+    print(command)   
+    for k, v in ssh2.ssh2.items():
+        hostname=ssh2.ssh2[k]['host']
         print (hostname)
         
-        #commands = ["banner motd ~\n\tThis is a secure site.\n\tOnly authorized users allowed.\n\t"+
-               # "For access please contact\n\tRockwood School District Technical Support."
-               # f"\n\tCM \n\t"+ hostname.replace('CMS', 'CM')+"\n\t5/21/2019\r ~",
-                #"hostname "+ hostname.replace('CMS', 'CM'),
-                #"do wr me"]
-        print(command)
+      #  commands = ["banner motd ~\n\tThis is a secure site.\n\tOnly authorized users allowed.\n\t"+
+       #         "For access please contact\n\tRockwood School District Technical Support."
+        #        f"\n\tEE \n\t"+ hostname.replace('ECE', 'EE')+"\n\t5/21/2019\r ~",
+         #       "hostname "+ hostname.replace('ECE', 'EE'),
+          #      "do wr me"]
         #connect to the switch using Netmiko. It is using k for key so it will go from 0 to the end for each entry
         try:
-            net_connect= Netmiko(**test_dict[k])
+            net_connect= Netmiko(**ssh2.ssh2[k])
             print(net_connect.find_prompt())
         #Send the commands as previously defined to the switch. This is from Netmiko
             output = net_connect.send_config_set(command)
